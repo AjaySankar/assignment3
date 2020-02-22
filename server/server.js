c42d43336faea419bdc31897ddca4af163facafe
Ajay Sankar <ajay0196@gmail.com>
2020-02-21 21:28:14 -0800

    Added getProducts, addProduct APIs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,25 +1,41 @@
+const find = require('lodash/find')
 const express = require('express')
 const { ApolloServer, gql } = require('apollo-server-express');
 
 const fs = require('fs')
 const typeDefs = fs.readFileSync('./schema.graphql',{encoding:'utf-8'})
+const defaultProductInfo = {id: 0, name: 'Shirts', category: '', price: '', image: ''}
+const products = [
+  {
+    id: 1,
+    name: 'Blue Shirt',
+    category: 'Shirts',
+    price: '12.30',
+    image: 'https://google.com/',
+  },
+  {
+    id: 2,
+    name: 'Black Jeans',
+    category: 'Jeans',
+    price: '17.99',
+    image: 'https://youtube.com/',
+  },
+];
 
 const resolvers = {
   Query: {
-    books: () => books,
+    products: () => products,
+    getProducts: () => products,
   },
+  Mutation: {
+    addProduct: (root, args) => {
+      const newProduct = Object.assign(defaultProductInfo, args)
+      products.push(newProduct)
+      const id = newProduct.id
+      return find(products, (product) => product.id == id)
+    }
+  }
 };
-
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-];
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
